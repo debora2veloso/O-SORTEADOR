9d4a15860ad70c33ee25d209a49da1ea1cb7ce27
debora2veloso <49910979+debora2veloso@users.noreply.github.com>
2020-02-04 15:54:20 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploConsole_SorteioNumerosNaoRepetidos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sorteando dez números que estejam entre o intervalo de 1 e 100.
+            Console.WriteLine(string.Join(", ", SorteiaNumerosSemRepetir(100, 1, 100)));
+            Console.ReadLine();
+        }
+
+        private static int[] SorteiaNumerosSemRepetir(int quantidade, int minimo, int maximo)
+        {
+            // Validações dos argumentos.
+            if (quantidade < 0)
+                throw new ArgumentException("Quantidade deve ser maior que zero.");
+            else if (quantidade > maximo + 1 - minimo)
+                throw new ArgumentException("Quantidade deve ser menor do que a diferença entre máximo e mínimo.");
+            else if (maximo <= minimo)
+                throw new ArgumentException("Máximo deve ser maior do que mínimo.");
+
+            List<int> numerosSorteados = new List<int>();
+            Random rnd = new Random();
+
+            while (numerosSorteados.Count < quantidade)
+            {
+                int numeroSorteado = rnd.Next(minimo, maximo + 1);
+
+                // Número já foi sorteado? Então sorteamos novamente até o número não ter sido sorteado ainda.
+                while (numerosSorteados.Contains(numeroSorteado))
+                    numeroSorteado = rnd.Next(minimo, maximo + 1);
+
+                numerosSorteados.Add(numeroSorteado);
+            }
+
+            return numerosSorteados.ToArray();
+        }
+    }
+}
---
+++ o sorteador.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+        ' Sorteando dez números que estejam entre o intervalo de 1 e 100.
+        Console.WriteLine(String.Join(", ", SorteiaNumerosSemRepetir(100, 1, 100)))
+        Console.ReadLine()
+    End Sub
+
+    Private Function SorteiaNumerosSemRepetir(quantidade As Integer, minimo As Integer, maximo As Integer) As Integer()
+        ' Validações dos argumentos.
+        If quantidade < 0 Then
+            Throw New ArgumentException("Quantidade deve ser maior que zero.")
+        ElseIf quantidade > maximo + 1 - minimo Then
+            Throw New ArgumentException("Quantidade deve ser menor do que a diferença entre máximo e mínimo.")
+        ElseIf maximo <= minimo Then
+            Throw New ArgumentException("Máximo deve ser maior do que mínimo.")
+        End If
+
+        Dim numerosSorteados As New List(Of Integer)()
+        Dim rnd As New Random()
+
+        While numerosSorteados.Count < quantidade
+            Dim numeroSorteado As Integer = rnd.[Next](minimo, maximo + 1)
+
+            ' Número já foi sorteado? Então sorteamos novamente até o número não ter sido sorteado ainda.
+            While numerosSorteados.Contains(numeroSorteado)
+                numeroSorteado = rnd.[Next](minimo, maximo + 1)
+            End While
+
+            numerosSorteados.Add(numeroSorteado)
+        End While
+
+        Return numerosSorteados.ToArray()
+    End Function
+End Module
